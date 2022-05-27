4ae11bacef62e0f3192c19a0900f1d7b6f7dbd0d
aklimaakhter <aklimawebd@gmail.com>
2022-05-27 18:43:49 +0600

    modal addad

--- src/Pages/Home/Part.js
+++ src/Pages/Home/Part.js
@@ -1,18 +1,19 @@
 import React from 'react';
 
-const Part = ({part}) => {
+const Part = ({ part, setBooking}) => {
     const {name, img,description,minimum_order_quantity,available_quantity,price}=part;
     return (
         <div class="card lg:max-w-lg bg-base-100 shadow-xl">
             <figure><img src={img} alt="Shoes" /></figure>
             <div class="card-body">
-                <h2 class="card-title">{name}</h2>
+                <h2 class="card-title text-secondary">{name}</h2>
                 <p>{description}</p>
                 <p>minimum_order_quantity:{minimum_order_quantity}</p>
                 <p>available_quantity:{available_quantity}</p>
                 <p>${price}</p>
-                <div class="card-actions justify-end">
-                    <button class="btn btn-primary">Buy Now</button>
+                <div class="card-actions justify-center">
+                    <label for="booking-modal" 
+                        onClick={() => setBooking(part)} class="btn btn-primary text-white uppercase">Buy Now</label>
                 </div>
             </div>
         </div>
--- src/Pages/Home/Parts.js
+++ src/Pages/Home/Parts.js
@@ -1,13 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Part from '../Home/Part';
+import BookingModal from '../Purchase/BookingModal';
 
 
 const Parts = () => {
-   
-    return (
-        <div>
-           
-        </div>
-    );
-};
+    const [parts, setParts] = useState([]);
 
+    const [booking, setBooking] = useState(null);
+
+    useEffect( () =>{
+        fetch('parts.json')
+        .then(res => res.json())
+        .then(data => setParts(data));
+    }, [])
+
+
+return (
+    <div>
+        <h2 className='text-4xl text-secondary font-bold text-center my-12'>Our Manufacturing Parts</h2>
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
+            {
+                parts.map(part => <Part
+                    key={part._id}
+                    part={part}
+                    setBooking={setBooking}
+                ></Part>)
+            }
+        </div>
+        {booking && <BookingModal 
+        booking={booking}
+        setBooking={setBooking}
+        ></BookingModal>}
+    </div>
+);
+};
 export default Parts;
